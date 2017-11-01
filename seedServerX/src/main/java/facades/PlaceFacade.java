f2d74709be441463f8d737dc30a6c9a5090a66ce
Mikkel <mikkel@mediagram.dk>
2017-11-01 09:12:11 +0100

    rest work

--- seedServerX/src/main/java/facades/PlaceFacade.java
+++ seedServerX/src/main/java/facades/PlaceFacade.java
@@ -2,39 +2,40 @@ package facades;
 
 import entity.Place;
 import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-class PlaceFacade implements IPlaceFacade
-{
+class PlaceFacade implements IPlaceFacade {
+    
+    private EntityManagerFactory emf;
 
     @Override
-    public void addEntityManagerFactory(EntityManagerFactory emf)
-    {
+    public void addEntityManagerFactory(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public boolean createPlace(Place place) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean createPlace(Place place)
-    {
+    public boolean removePlace(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean removePlace(int id)
-    {
+    public Place findPlace(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Place findPlace(int id)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<Place> listAllPlaces()
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Place> listAllPlaces() {
+        EntityManager em = emf.createEntityManager();
+        List<Place> results = em.createQuery("SELECT p FROM Place p").getResultList();
+        return results;
     }
     
+    
+
 }
