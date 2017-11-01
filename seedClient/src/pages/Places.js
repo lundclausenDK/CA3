036ff944b34750a696ec09ab3b383542bce7db73
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-11-01 11:55:19 +0100

    sorts and search done!

--- seedClient/src/pages/Places.js
+++ seedClient/src/pages/Places.js
@@ -5,7 +5,10 @@ import React from 'react';
 export default class Place extends React.Component {
     constructor() {
         super();
-        this.state = {places: []};
+        this.state = {
+            places: [],
+            view: []
+        };
         this.getData = this.getData.bind(this);
         this.getData();
     }
@@ -16,9 +19,42 @@ export default class Place extends React.Component {
                 return response.json()
             }).then(function (data) {
             console.log(data);
-            this.setState({places: data});
+            this.setState({places: data, view: data});
         }.bind(this));
 
+    };
+
+    getSearch = (e) => {
+        const name = document.getElementById("searchText").value;
+        let viewList = this.state.places.filter((place)=>{
+            if(place.name.includes(name)){
+                return place;
+            }
+        });
+        this.setState({view: viewList});
+        e.preventDefault();
+    };
+
+    sortOnName = (e) => {
+        let sorted = this.state.places.sort((a, b)=>{
+            if(a.name < b.name){
+                return -1;
+            }else if(b.name.toLocaleLowerCase() < a.name.toLocaleLowerCase()){
+                return 1;
+            }
+        });
+        this.setState({view: sorted});
+        e.preventDefault();
+    };
+
+    sortOnRating = (e) => {
+        let sorted = this.state.places.sort((a, b)=>{
+            if(a.rating < b.rating) return -1;
+            if(a.rating > b.rating) return 1;
+            return 0;
+        });
+        this.setState({view: sorted});
+        e.preventDefault();
     };
 
     render() {
@@ -27,10 +63,16 @@ export default class Place extends React.Component {
 
                 <h2>Places</h2>
 
-                {this.state.places.map((item) => (
+                <form>
+                    <input id="searchText" type="text" placeholder="Type the name here"/>
+                    <button onClick={this.getSearch}>submit</button>
+                </form>
+                <button onClick={this.sortOnName}>Sort on Name</button>
+                <button onClick={this.sortOnRating}>Sort on Rating</button>
+                {this.state.view.map((item) => (
                     <div className="places-container clearfix">
 
-                        <div className="left image"><img src={item.url} /></div>
+                        <div className="left image"><img src={item.url}/></div>
                         <div className="bold">{item.name}</div>
                         <div>{item.description}</div>
                         <div>{item.street}</div>
