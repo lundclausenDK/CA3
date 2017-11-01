24943cc9d98f799b9c91ba2d9183cdbf71d640a5
Peter Rasmussen <leonhardt_88@live.dk>
2017-11-01 08:24:40 +0100

    client side react

---
+++ seedClient/src/pages/Summerhouses.js
@@ -0,0 +1,49 @@
+
+
+
+import React from 'react';
+
+
+
+const URL = require("../../package.json").serverURL;
+
+
+export default class SummerHouse extends React.Component{
+    constructor(){
+        super();
+
+    }
+    getdata(){
+
+        let resFromFirstPromise=null;
+
+        let method = "GET";
+        let headers = {
+            "Content-type": "Application/json"};
+        let options = {
+            method,
+            headers
+          }
+
+
+        fetch(URL + "api/demouser", options)
+        .then((res) => {
+          resFromFirstPromise = res;
+          return res.json();
+        }).then((data) => {
+          //errorChecker(resFromFirstPromise,data);
+          
+        }).catch(err => {
+          console.log(JSON.stringify(err))
+          
+        })
+    }
+
+    render(){
+        return(
+            <div>heloooo</div>
+
+
+        )
+    }
+}
