dc4b59b4ad358bc03695a2d986d323a110d39b0f
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-11-01 12:17:37 +0100

    only users can see page for úsers

--- seedClient/src/pages/TopMenu.js
+++ seedClient/src/pages/TopMenu.js
@@ -31,7 +31,7 @@ class TopMenu extends Component {
             <ul className="nav navbar-nav">
               <li><Link to="">Home</Link></li>
               <li><Link to="/about">About</Link></li>
-              <li><Link to="/user">Page for Users </Link></li>
+                {this.state.isUser && <li><Link to="/user">Page for Users </Link></li>}
               
               <li><Link to="/places">Places</Link></li>
               {this.state.isAdmin && <li><Link to="/admin">Page for Admins</Link></li>}
