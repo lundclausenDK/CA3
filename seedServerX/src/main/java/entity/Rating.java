2f4ec56ccbfb92a9104c90e6161783f3dab8e748
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-11-03 14:08:27 +0100

    Now working database to front-end with places

--- seedServerX/src/main/java/entity/Rating.java
+++ seedServerX/src/main/java/entity/Rating.java
@@ -1,20 +1,14 @@
 package entity;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
 public class Rating implements Serializable {
-
-    @ManyToOne
-    private Place place;
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -39,14 +33,6 @@ public class Rating implements Serializable {
 
     public void setUser(User user) {
         this.user = user;
-    }
-
-    public Place getPlace() {
-        return place;
-    }
-
-    public void setPlace(Place place) {
-        this.place = place;
     }
     
     public int getId() {
