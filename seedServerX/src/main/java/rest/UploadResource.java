ab74179fbc82ae9521581ce25451fe7c17b63a00
Peter Rasmussen <leonhardt_88@live.dk>
2017-11-03 08:37:14 +0100

    uploadresource: rest endpoint for pictures

---
+++ seedServerX/src/main/java/rest/UploadResource.java
@@ -0,0 +1,78 @@
+package rest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+@Path("upload")
+public class UploadResource {
+
+    //public static final String FILE_LOCATION = "D:\\photobucket\\";
+    public String path;
+    
+    
+    UploadResource() {
+            if (!System.getProperty("os.name").equals("Windows")) {
+                path = "/etc/img";
+            }
+            File folder = new File(path);
+            folder.mkdir();
+        }
+    
+    
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of UploadResource
+     */
+    
+
+    /**
+     * Retrieves representation of an instance of
+     * com.mycompany.photoupload.UploadResource
+     *
+     * @return an instance of java.lang.String
+     */
+    @Path("/file")
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(@DefaultValue("") @FormDataParam("user") String user,
+            @FormDataParam("file") InputStream file,
+            @FormDataParam("file") FormDataContentDisposition fileDisposition) throws IOException {
+
+        
+        
+
+        System.out.println("image loaded : " + user);
+        String fileName = fileDisposition.getFileName();
+        saveFile(file, fileName);
+        String status = "{\"status\":\"uploaded\"}";
+        return Response.ok(status).build();
+    }
+
+    private void saveFile(InputStream is, String fileLocation) throws IOException {
+
+        String location = path + fileLocation;
+        try (OutputStream os = new FileOutputStream(new File(location))) {
+            byte[] buffer = new byte[256];
+            int bytes = 0;
+            while ((bytes = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytes);
+            }
+        }
+    }
+
+}
