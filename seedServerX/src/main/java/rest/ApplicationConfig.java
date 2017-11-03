d413bb53d26bdfcfc80566918b40deb94886b98f
Mikkel <mikkel@mediagram.dk>
2017-11-03 15:38:29 +0100

    fixy joe was here

--- seedServerX/src/main/java/rest/ApplicationConfig.java
+++ seedServerX/src/main/java/rest/ApplicationConfig.java
@@ -8,42 +8,19 @@ import org.glassfish.jersey.media.multipart.MultiPartFeature;
 public class ApplicationConfig extends Application {
 
     @Override
-    public Set<Class<?>> getClasses()
-    {
+    public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
         addRestResourceClasses(resources);
         resources.add(MultiPartFeature.class);
         return resources;
     }
 
-<<<<<<< HEAD
-  /**
-   * Do not modify addRestResourceClasses() method.
-   * It is automatically populated with
-   * all resources defined in the project.
-   * If required, comment out calling this method in getClasses().
-   */
-  private void addRestResourceClasses(Set<Class<?>> resources) {
-    resources.add(cors.CorsRequestFilter.class);
-    resources.add(cors.CorsResponseFilter.class);
-    resources.add(httpErrors.GenericExceptionMapper.class);
-    resources.add(httpErrors.NotFoundExceptionMapper.class);
-    resources.add(rest.Admin.class);
-    resources.add(rest.All.class);
-    resources.add(rest.PlacesResource.class);
-    resources.add(rest.Register.class);
-    resources.add(rest.UploadResource.class);
-    resources.add(rest.User.class);
-    resources.add(rest.Users.class);
-    resources.add(security.JWTAuthenticationFilter.class);
-=======
     /**
      * Do not modify addRestResourceClasses() method. It is automatically
      * populated with all resources defined in the project. If required, comment
      * out calling this method in getClasses().
      */
-    private void addRestResourceClasses(Set<Class<?>> resources)
-    {
+    private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(cors.CorsRequestFilter.class);
         resources.add(cors.CorsResponseFilter.class);
         resources.add(httpErrors.GenericExceptionMapper.class);
@@ -57,7 +34,7 @@ public class ApplicationConfig extends Application {
         resources.add(rest.UserControl.class);
         resources.add(rest.Users.class);
         resources.add(security.JWTAuthenticationFilter.class);
-        resources.add(security.Login.class)
+        resources.add(security.Login.class);
         resources.add(security.NotAuthorizedExceptionMapper.class);
         resources.add(security.RolesAllowedFilter.class);
     }
