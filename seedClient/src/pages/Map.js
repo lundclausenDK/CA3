dd2d0c4606d21dfe4021bcf257b02ddc3ac1ef6e
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-11-22 09:21:14 +0100

    map with array

--- seedClient/src/pages/Map.js
+++ seedClient/src/pages/Map.js
@@ -11,18 +11,25 @@ const MyMapComponent = compose(
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `600px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
+        geoList: ["56.058363,12.067663","55.5346844,12.067663", "56.021966,10.2598323"]
     }),
     withScriptjs,
-    withGoogleMap
+    withGoogleMap,
 )((props) =>
     <GoogleMap
         api="AIzaSyAPL2w6-qEYb83Da8rcYbaXVERKya6kNy0"
         defaultZoom={8}
         defaultCenter={{ lat: 56.15, lng: 10.5 }}
     >
-        {props.isMarkerShown && <Marker position={{ lat: 56.058363, lng: 12.067663 }} onClick={props.onMarkerClick} />}
+
+        {props.geoList.map((geo)=>{
+            let geosplittet = geo.split(',');
+            return <Marker position={{ lat: geosplittet[0], lng: geosplittet[1]}} onClick={props.onMarkerClick}/>
+        })}
+
+        /*{props.isMarkerShown && <Marker position={{ lat: 56.058363, lng: 12.067663 }} onClick={props.onMarkerClick} />}
         {props.isMarkerShown && <Marker position={{ lat: 55.5346844, lng: 11.8525492 }} onClick={props.onMarkerClick} />}
-        {props.isMarkerShown && <Marker position={{ lat: 56.021966, lng: 10.2598323 }} onClick={props.onMarkerClick} />}
+        {props.isMarkerShown && <Marker position={{ lat: 56.021966, lng: 10.2598323 }} onClick={props.onMarkerClick} />}*/
     </GoogleMap>
 )
 
