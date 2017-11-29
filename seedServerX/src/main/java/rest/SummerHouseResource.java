c5b17c9cb548711af7e7dc2b4231e320c895d1bb
Joachim Hansen <craciez@live.dk>
2017-11-29 10:46:02 +0100

    SummerhousRestTest added and working!

--- seedServerX/src/main/java/rest/SummerHouseResource.java
+++ seedServerX/src/main/java/rest/SummerHouseResource.java
@@ -1,8 +1,10 @@
 package rest;
 
-import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -20,7 +22,8 @@ public class SummerHouseResource {
 "        \"description\": \"The most beautiful summerhouse you've ever seen\",\n" +
 "        \"city\": \"The big city\",\n" +
 "        \"picture\": \"https://www.feline.dk/Media/Feline_Holidays_DK/_Profiles/8ce764d7/1e6be63/Sommerhus%20Als_130-F09140.jpg?v=636069406726870339\",\n" +
-"        \"price\": \"222\"\n" +
+"        \"price\": \"222\",\n" +
+"        \"geo\": \"77.55,53.01\"\n"+
 "   },\n" +
 "    {\n" +
 "        \"id\": \"2\",\n" +
@@ -29,9 +32,32 @@ public class SummerHouseResource {
 "        \"description\": \"The best summerhouse ever\",\n" +
 "        \"city\": \"The beautiful city \",\n" +
 "        \"picture\": \"https://st.hzcdn.com/simgs/dba10887054a0dc3_4-9552/modern-exterior.jpg\",\n" +
-"        \"price\": \"300\"\n" +
+"        \"price\": \"300\",\n" +
+"        \"geo\": \"77.53,53.02\"\n"+
 "   }\n" +
 "]"; 
   }
+  
+  @Path("{id}")
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String getSpecific(@PathParam("id") int id){
+      return "    {\n" +
+"        \"id\": \"1\",\n" +
+"        \"title\": \"The Greatest of the all\",\n" +
+"        \"address\": \"The nice street\",\n" +
+"        \"description\": \"The most beautiful summerhouse you've ever seen\",\n" +
+"        \"city\": \"The big city\",\n" +
+"        \"picture\": \"https://www.feline.dk/Media/Feline_Holidays_DK/_Profiles/8ce764d7/1e6be63/Sommerhus%20Als_130-F09140.jpg?v=636069406726870339\",\n" +
+"        \"price\": \"222\",\n" +
+"        \"geo\": \"77.55,53.01\"\n"+
+"   }\n";
+  }
  
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String postHouse(String content){
+      return "{\"message\": \"done\"}";
+  }
 }
---
+++ seedServerX/src/test/java/test/SummerhouseRestTest.java
@@ -0,0 +1,85 @@
+package test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import io.restassured.parsing.Parser;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author craci
+ */
+public class SummerhouseRestTest {
+    
+    public SummerhouseRestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8080;
+        RestAssured.basePath = "/seedMaven";
+        RestAssured.defaultParser = Parser.JSON;
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    
+    //@Ignore
+    @Test
+    public void getFirstID() {
+        given().when().get("/api/summerhouses").then().body("id[0]", equalTo("1"));
+    }
+    
+    //@Ignore
+    @Test
+    public void getAllHomeData() {
+        String api = "/api/summerhouses";
+        given().when().get(api).then().body("id", hasItems("1", "2"))
+                .body("title", hasItems("The Greatest of the all","The black summerhouse"))
+                .body("address", hasItems("The nice street","The super nice street"))
+                .body("geo", hasItems("77.55,53.01", "77.53,53.02"));
+    }
+    
+    //@Ignore
+    @Test
+    public void getSpecificHomeData(){
+        String api ="/api/summerhouses/{id}";
+        given().pathParams("id", 1).when().get(api).then()
+                .body("id", equalTo("1"));
+    }
+    
+    @Test
+    public void putMethodTesting(){
+        String api = "/api/summerhouses";
+        String json = "{"
+                + "'title': 'Super summerhouse',"
+                + "'address': 'gadevejen 32',"
+                + "'desctiption': 'Gammel skøn klassiker',"
+                + "'city': 'Bystaden',"
+                + "'picture': 'hus.jpg',"
+                + "'price': '3000',"
+                + "'geo': '44.55,55.44'}";
+        given().contentType("application/json").body(json).when().post(api).then().body("message", equalTo("done"));
+    }
+}
