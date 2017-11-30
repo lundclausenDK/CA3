381461cefe0dc2324c3d33e606dee0560d3be1e7
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-11-30 14:41:46 +0100

    add house test works now with the real backend

--- seedServerX/src/main/java/rest/SummerHouseResource.java
+++ seedServerX/src/main/java/rest/SummerHouseResource.java
@@ -3,11 +3,13 @@ package rest;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import entity.Booking;
 import entity.Home;
 import facades.CollectiveFacadeFactory;
 import facades.ICollectiveFacade;
 import java.util.List;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -19,8 +21,8 @@ import javax.ws.rs.core.MediaType;
 @Path("summerhouses")
 public class SummerHouseResource {
 
-    ICollectiveFacade uf = CollectiveFacadeFactory.getInstance();
-    Gson gs = new Gson();
+    private ICollectiveFacade uf = CollectiveFacadeFactory.getInstance();
+    private Gson gs = new Gson();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -57,14 +59,55 @@ public class SummerHouseResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getSpecific(@PathParam("id") int id) {
-        return null;
+        try {
+            Home home = uf.findHomeById(id);
+            JsonObject homeDetails = new JsonObject();
+            homeDetails.addProperty("id", home.getId());
+            homeDetails.addProperty("title", home.getName());
+            homeDetails.addProperty("address", home.getAddress());
+            homeDetails.addProperty("description", home.getDescription());
+            homeDetails.addProperty("city", home.getCity());
+            homeDetails.addProperty("picture", "hej");
+            homeDetails.addProperty("price", home.getPrice());
+            homeDetails.addProperty("geo", home.getGeo());
+
+            JsonArray bookings = new JsonArray();
+            for (Booking booking : home.getBookings()) {
+                JsonObject newBooking = new JsonObject();
+                newBooking.addProperty("startDate", booking.getStartTime());
+                newBooking.addProperty("endDate", booking.getEndTime());
+                newBooking.addProperty("user", booking.getRenter().getUserName());
+                bookings.add(newBooking);
+            }
+            homeDetails.add("bookings", bookings);
+            return homeDetails.toString();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("Admin")
     public String postHouse(String content) {
-        return "{\"message\": \"done\"}";
+        try {
+            JsonObject json = new JsonParser().parse(content).getAsJsonObject();
+            String title = json.get("title").getAsString();
+            String address = json.get("address").getAsString();
+            String description = json.get("description").getAsString();
+            String city = json.get("city").getAsString();
+            int price = json.get("price").getAsInt();
+            String geo = json.get("geo").getAsString();
+            
+            Home home = new Home(title, description, address, price, city, geo, price);
+            uf.addHome(home);
+            
+            return "{\"message\": \"done\"}";
+        }catch(Exception e){
+            return "{\"error\": \"Couldn't add home\"}";
+        }
+
     }
 
     @Path("rent/{id}")
--- seedServerX/src/test/java/test/SummerhouseRestTest.java
+++ seedServerX/src/test/java/test/SummerhouseRestTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import deploy.DeploymentConfiguration;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import org.junit.After;
@@ -8,26 +9,64 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import io.restassured.parsing.Parser;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import org.apache.catalina.LifecycleException;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
 import org.junit.Ignore;
+import security.Secret;
+import test.utils.EmbeddedTomcat;
 
 /**
  *
  * @author craci
  */
 
-@Ignore
 public class SummerhouseRestTest {
     
+    private static final int SERVER_PORT = 9999;
+    private static final String APP_CONTEXT = "/seed";
+    private static EmbeddedTomcat tomcat;
+    
     public SummerhouseRestTest() {
+        String content = "tokenSecret=c6hFJOYY75765444EEEEvgTdeMnbV30h";
+
+        Properties prop = new Properties();
+        try (InputStream input = new ByteArrayInputStream(content.getBytes())) {
+            prop.load(input);
+            Secret.SHARED_SECRET = prop.getProperty("tokenSecret").getBytes();
+        } catch (IOException ex) {
+            Logger.getLogger(DeploymentConfiguration.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    private static String securityToken;
+    
+    private static void login(String username, String password) {
+        String json = String.format("{username: \"%s\", password: \"%s\"}", username, password);
+        System.out.println(json);
+        securityToken = given()
+                .contentType("application/json")
+                .body(json)
+                .when().post("/api/login")
+                .then()
+                .extract().path("token");
+        System.out.println("Token: " + securityToken);
     }
     
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws ServletException, MalformedURLException, LifecycleException{
+        tomcat = new EmbeddedTomcat();
+        tomcat.start(SERVER_PORT, APP_CONTEXT);
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8080;
-        RestAssured.basePath = "/seedMaven";
+        RestAssured.port = SERVER_PORT;
+        RestAssured.basePath = APP_CONTEXT;
         RestAssured.defaultParser = Parser.JSON;
     }
     
@@ -47,42 +86,41 @@ public class SummerhouseRestTest {
     // The methods must be annotated with annotation @Test. For example:
     //
     
-    //@Ignore
     @Test
     public void getFirstID() {
-        given().when().get("/api/summerhouses").then().body("id[0]", equalTo("1"));
+        given().when().get("/api/summerhouses").then().body("id[0]", equalTo(1));
     }
     
     //@Ignore
     @Test
     public void getAllHomeData() {
         String api = "/api/summerhouses";
-        given().when().get(api).then().body("id", hasItems("1", "2"))
-                .body("title", hasItems("The Greatest of the all","The black summerhouse"))
-                .body("address", hasItems("The nice street","The super nice street"))
-                .body("geo", hasItems("77.55,53.01", "77.53,53.02"));
+        given().when().get(api).then().body("id", hasItems(1, 2))
+                .body("title", hasItems("Beautiful home","Second Most Beautiful Home"))
+                .body("address", hasItems("Main Street","Second Street"))
+                .body("geo", hasItems("44.33,22.44", "55.22,55.36"));
     }
     
-    //@Ignore
     @Test
     public void getSpecificHomeData(){
         String api ="/api/summerhouses/{id}";
         given().pathParams("id", 1).when().get(api).then()
-                .body("id", equalTo("1"));
+                .body("id", equalTo(1));
     }
     
     @Test
     public void putMethodTesting(){
+        login("admin", "test");
         String api = "/api/summerhouses";
         String json = "{"
-                + "'title': 'Super summerhouse',"
-                + "'address': 'gadevejen 32',"
-                + "'desctiption': 'Gammel skøn klassiker',"
-                + "'city': 'Bystaden',"
-                + "'picture': 'hus.jpg',"
-                + "'price': '3000',"
-                + "'geo': '44.55,55.44'}";
-        given().contentType("application/json").body(json).when().post(api).then().body("message", equalTo("done"));
+                + "\"title\": \"Super summerhouse\","
+                + "\"address\": \"gadevejen 32\","
+                + "\"description\": \"Gammel skøn klassiker\","
+                + "\"city\": \"Bystaden\","
+                + "\"picture\": \"hus.jpg\","
+                + "\"price\": \"3000\","
+                + "\"geo\": \"44.55,55.44\"}";
+        given().contentType("application/json").header("Authorization", "Bearer " + securityToken).body(json).when().post(api).then().body("message", equalTo("done"));
     }
     
     @Test
@@ -90,9 +128,9 @@ public class SummerhouseRestTest {
         int home_id = 2;
         String api = "/api/summerhouses/rent/{id}";
         String json = "{"
-                + "'start': '10500500',"
-                + "'end': '20500500',"
-                + "'user_id': '1'}";
+                + "\"start\": \"10500500\","
+                + "\"end\": \"20500500\","
+                + "\"user_id\": \"1\"}";
         given().pathParams("id", home_id).contentType("application/json").body(json).when().post(api).then().body("message", equalTo("reserved: " + home_id));
     }
 }
