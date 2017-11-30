c37387eef50f50c35a8ec42785108c0707c523ba
Mikkel <mikkel@mediagram.dk>
2017-11-30 14:52:39 +0100

    react work

---
+++ seedClient/src/pages/AddSummerhouse.js
@@ -0,0 +1,95 @@
+import React from "react";
+import fetchHelper from "../facades/fetchHelpers";
+import auth from '../authorization/auth';
+
+const URL = require("../../package.json").serverURL;
+
+export default class AddPlace extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            name: null,
+            street: null,
+            city: null,
+            zip: null,
+            desc: null,
+            rating: null,
+        }
+
+        auth.initDataFromToken();
+        if (auth.isloggedIn) {
+            this.state.userName = auth.userName;
+        }
+
+        this.addData = this.addData.bind(this);
+    }
+
+    returnValue = (e) => {
+        let id = e.target.id;
+        let value = e.target.value;
+
+        this.setState({ [id]: value });
+        e.preventDefault();
+    };
+
+    addData = () => {
+
+        const options = fetchHelper.makeOptions("POST", true, {
+            name: this.state.name,
+            street: this.state.street,
+            city: this.state.city,
+            zip: this.state.zip,
+            desc: this.state.description
+        });
+
+        fetch(URL + "api/summerhouses", options);
+    };
+
+    pushPlaceToServer = () => {
+
+        /*
+        const place = { name: this.state.name, street: this.state.street, city: this.state.city, zip: this.state.zip, desc: this.state.desc, file: this.state.file };
+        const options = fetchHelper.makeOptions("POST", true, place);
+
+        fetch(URL + "api/upload/placeUpload", options);
+        */
+
+        var input = document.querySelector('input[type="file"]');
+        var data = new FormData();
+
+        data.append("name", this.state.name);
+        data.append("street", this.state.street);
+        data.append("city", this.state.city);
+        data.append("zip", this.state.zip);
+        data.append("desc", this.state.desc);
+        data.append("userName", this.state.userName);
+
+        data.append('file', input.files[0]);
+        fetch(URL + 'api/upload/placeUpload', {
+            method: 'POST',
+            body: data, headers: {'Authorization': `Bearer ${sessionStorage.token}`}
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Add new summerhouse</h2>
+                <form className="ca3">
+                    <input placeholder="Type name here" value={this.state.name} onChange={this.returnValue} id="name" />
+                    <input placeholder="Type street here" value={this.state.street} onChange={this.returnValue} id="street" />
+                    <input placeholder="Type city here" value={this.state.city} onChange={this.returnValue} id="city" />
+                    <input type="number" placeholder="Type zip here" value={this.state.zip} onChange={this.returnValue} id="zip" />
+                    <textarea placeholder="Please type description" value={this.state.description} onChange={this.returnValue} id="desc" />
+                    <label>Select File</label><input type="file" id="file" /> <br /><br />
+                    <button onClick={this.pushPlaceToServer}>Submit</button>
+                </form>
+            </div>
+        );
+
+    }
+
+
+}
--- seedClient/src/pages/TopMenu.js
+++ seedClient/src/pages/TopMenu.js
@@ -1,64 +1,67 @@
-import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import React, {Component} from 'react'
+import {Link} from "react-router-dom";
 import auth from '../authorization/auth'
 
 class TopMenu extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { loggedIn: auth.isloggedIn, userName: auth.userName, isUser: auth.isUser, isAdmin: auth.isAdmin }
-  }
+    constructor(props) {
+        super(props);
+        this.state = {loggedIn: auth.isloggedIn, userName: auth.userName, isUser: auth.isUser, isAdmin: auth.isAdmin}
+    }
 
-  loginStatus = (status, userName, isUser, isAdmin) => {
-    this.setState({ loggedIn: status, userName, isUser, isAdmin });
-  }
+    loginStatus = (status, userName, isUser, isAdmin) => {
+        this.setState({loggedIn: status, userName, isUser, isAdmin});
+    }
 
-  componentDidMount() {
-    auth.setLoginObserver(this.loginStatus);
-  }
+    componentDidMount() {
+        auth.setLoginObserver(this.loginStatus);
+    }
 
-  render() {
+    render() {
 
-    const logInStatus = this.state.loggedIn ? "Logged in as: " + this.state.userName : "";
+        const logInStatus = this.state.loggedIn ? "Logged in as: " + this.state.userName : "";
 
-    return (
-      <div>
-        <nav className="navbar navbar-default" >
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="/" style={{ pointerEvents: "none" }}></a>
+        return (
+            <div>
+                <img src="/redfox.png" className="headerIcon"/><h1 className="headerFont"><span>RedFox</span> - Invest n' rent</h1>
+
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="/" style={{pointerEvents: "none"}}></a>
+                        </div>
+                        <ul className="nav navbar-nav">
+                            <li><Link to="">Home</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            {this.state.isUser && <li><Link to="/add-new-place">Add new place</Link></li>}
+
+                            <li><Link to="/places">Places</Link></li>
+                            <li><Link to="/summerhouses">Summerhouses</Link></li>
+
+                            {this.state.isAdmin && <li><Link to="/admin">Page for Admins</Link></li>}
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li className="navbar-text" style={{color: "steelBlue"}}>{logInStatus}</li>
+                            <li>
+                                {this.state.loggedIn ?
+                                    (
+                                        <Link to="/logout"><span className="glyphicon glyphicon-log-out"></span> Logout</Link>
+                                    ) :
+                                    (
+                                        <li>
+                                            <Link to="/login">
+                                                <span className="glyphicon glyphicon-log-in"></span> Login</Link> |
+                                            <Link to="/signup"><span className="glyphicon glyphicon-signin"></span>Signup</Link>&nbsp;&nbsp;
+                                        </li>
+                                    )}
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
             </div>
-            <ul className="nav navbar-nav">
-              <li><Link to="">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-                {this.state.isUser && <li><Link to="/add-new-place">Add new place</Link></li>}
-              
-              <li><Link to="/places">Places</Link></li>
-              <li><Link to="/summerhouses">Summerhouses</Link></li>
-
-              {this.state.isAdmin && <li><Link to="/admin">Page for Admins</Link></li>}
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li className="navbar-text" style={{ color: "steelBlue" }}>{logInStatus}</li>
-              <li>
-                {this.state.loggedIn ?
-                  (
-                    <Link to="/logout"><span className="glyphicon glyphicon-log-out"></span> Logout</Link>
-                  ) :
-                  (
-                    <li>
-                      <Link to="/login">
-                        <span className="glyphicon glyphicon-log-in"></span> Login</Link> | <Link to="/signup"><span className="glyphicon glyphicon-signin"></span>Signup</Link>&nbsp;&nbsp;
-                    </li>
-                  )}
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-      </div>
-    )
-  }
+        )
+    }
 }
 
 
