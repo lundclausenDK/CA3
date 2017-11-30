3988fc2e7276bd56497b72b5a53f43d8d3bf9caa
Bade-PC\Bade <benjaminlrasmussen@gmail.com>
2017-11-30 16:00:25 +0100

    Merge

--- seedServerX/src/main/java/entity/Role.java
+++ seedServerX/src/main/java/entity/Role.java
@@ -1,12 +1,9 @@
 package entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 @Entity(name = "USER_ROLE")
 public class Role implements Serializable {
