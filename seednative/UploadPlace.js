8fd0651d144f78a7ae84bc08f9c67a4a1a8036f1
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-12-04 12:08:18 +0100

    minor change to label in profile tab

--- seednative/UploadPlace.js
+++ seednative/UploadPlace.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 export default class UploadPlace extends Component{
-    static navigationOptions = {title: 'Upload a Place'};
+    static navigationOptions = {tabBarLabel: 'Profile', title: 'Upload a Place'};
     render(){
         return(
             <View>
